e86953252d2ce1ea4405250414b28bc72aa45f1e
doston <dos400dos400@gmail.com>
2022-02-24 11:49:02 +0500

    add adapters to home section users

--- app/src/main/java/uz/hamroev/tulipsofuzbekistan/activity/TulipsActivity.kt
+++ app/src/main/java/uz/hamroev/tulipsofuzbekistan/activity/TulipsActivity.kt
@@ -2,6 +2,9 @@ package uz.hamroev.tulipsofuzbekistan.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.Navigation
+import uz.hamroev.tulipsofuzbekistan.R
 import uz.hamroev.tulipsofuzbekistan.cache.Cache
 import uz.hamroev.tulipsofuzbekistan.databinding.ActivityTulipsBinding
 
@@ -10,9 +13,16 @@ class TulipsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTulipsBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
 
+        Cache.init(this)
 
 
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.my_nav_host_fragment).navigateUp()
     }
 }
